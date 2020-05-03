451c38f52ffd80e7e3ff90332b9e30e5dcfdb666
Daksh <daksh@technowolf.in>
2020-05-03 23:12:07 +0530

    add extensions for Byte since 'shl', 'and' operations that return Int was not available in standard kotlin extensions.

---
+++ app/src/main/java/in/technowolf/nyx/utils/Extension.kt
@@ -0,0 +1,10 @@
+package `in`.technowolf.nyx.utils
+
+object Extension {
+    infix fun Byte.shl(that: Int): Int = this.toInt().shl(that)
+    infix fun Byte.shr(that: Int): Int = this.toInt().shr(that)
+
+
+    infix fun Byte.and(that: Int): Int = this.toInt().and(that)
+    infix fun Int.and(that: Byte): Int = this.and(that.toInt())
+}
