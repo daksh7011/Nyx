824da3081a336e24edc203305e8e173162e7ef37
Daksh <daksh@technowolf.in>
2020-05-03 23:12:53 +0530

    implement steganography demonstration on android views (total spaghetti)

--- app/src/main/java/in/technowolf/nyx/MainActivity.kt
+++ app/src/main/java/in/technowolf/nyx/MainActivity.kt
@@ -1,10 +1,14 @@
 package `in`.technowolf.nyx
 
 import `in`.technowolf.nyx.core.MagicWand
+import `in`.technowolf.nyx.core.Stego
 import `in`.technowolf.nyx.databinding.ActivityMainBinding
 import `in`.technowolf.nyx.utils.binding
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +17,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val asd = MagicWand.encrypt("Beep Beep, Boop Beep!", "12345")
-        binding.tvHelloWorld.text = MagicWand.decrypt(asd, "12345")
+        val asd = MagicWand.encrypt(binding.editText.text.toString(), "12345")
+        val bm = (binding.raw.drawable as BitmapDrawable).bitmap
+        val stego = Stego().encodeMessage(listOf(bm), asd!!)
+        binding.finished.setImageBitmap(stego[0])
+        binding.btnDecrypt.setOnClickListener {
+            val decodedImage = (binding.finished.drawable as BitmapDrawable).bitmap
+            val decode = Stego().decodeMessage(listOf(decodedImage))
+            Log.e(javaClass.simpleName, MagicWand.decrypt(decode, "12345"))
+        }
     }
 }
