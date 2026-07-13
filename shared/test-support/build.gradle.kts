e4af0b3292b0f54a99a1cdebb4937b3d9e52e264
Daksh <daksh@technowolf.in>
2026-07-13 19:04:10 +0530

    feat(test-support): FakeClock and DeterministicIdGenerator

--- shared/test-support/build.gradle.kts
+++ shared/test-support/build.gradle.kts
@@ -1,3 +1,21 @@
 plugins {
     id("nyx.kmp.library")
 }
+
+kotlin {
+    sourceSets {
+        all {
+            languageSettings {
+                optIn("kotlin.time.ExperimentalTime")
+                optIn("kotlinx.coroutines.ExperimentalCoroutinesApi")
+            }
+        }
+        commonMain.dependencies {
+            api(projects.shared.data)
+            api(libs.kotlinx.datetime)
+            api(libs.kotlinx.coroutines.core)
+            api(libs.sqldelight.runtime)
+            api(libs.sqldelight.async.extensions)
+        }
+    }
+}
