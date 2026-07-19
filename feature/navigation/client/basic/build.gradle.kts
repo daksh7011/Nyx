3cfbec26a01b6a0c939db1aa139e6f7dc464c7a7
Daksh <daksh@technowolf.in>
2026-07-19 17:00:55 +0530

    chore(navigation): drop unused koin deps from basic build

--- feature/navigation/client/basic/build.gradle.kts
+++ feature/navigation/client/basic/build.gradle.kts
@@ -7,9 +7,7 @@ kotlin {
         commonMain.dependencies {
             api(projects.feature.navigation.client.api)
             api(projects.feature.common.client.api)
-            api(projects.feature.common.client.koin)
             implementation(projects.shared.designLibrary)
-            implementation(libs.koin.compose.viewmodel)
         }
     }
 }
